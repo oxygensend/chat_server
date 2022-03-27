c4ff3a7eda35119245efe5940d4a1709d07baec1
Szymon Berdzik <szymon19314@wp.pl>
2022-03-27 22:31:52 +0200

    preparing for prod mode

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,10 +5,11 @@ namespace App\Providers;
 use App\Models\Room;
 use App\Observers\RoomObserver;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
+
     /**
      * Register any application services.
      *
@@ -26,6 +27,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
         //
         Model::unguard();
     }
